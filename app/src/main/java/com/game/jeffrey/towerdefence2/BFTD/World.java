05ce01cedf339e2b2a60bbf10d319b8e3903cde8
Jeffrey <jeffrey@kadnet.dk>
2016-11-21 09:49:56 +0100

    world renderer intial

--- app/src/main/java/com/game/jeffrey/towerdefence2/BFTD/World.java
+++ app/src/main/java/com/game/jeffrey/towerdefence2/BFTD/World.java
@@ -3,4 +3,8 @@ package com.game.jeffrey.towerdefence2.BFTD;
 
 public class World
 {
+    public static float MIN_X = 0;
+    public static float MIN_Y = 32;
+    public static float MAX_X = 319;
+    public static float MAX_Y = 479;
 }
---
+++ app/src/main/java/com/game/jeffrey/towerdefence2/BFTD/WorldRenderer.java
@@ -0,0 +1,22 @@
+package com.game.jeffrey.towerdefence2.BFTD;
+
+
+import com.game.jeffrey.towerdefence2.GameEngine;
+
+public class WorldRenderer
+{
+    GameEngine game;
+    World world;
+
+    public WorldRenderer(GameEngine game, World world)
+    {
+        this.game = game;
+        this.world = world;
+        //load bitmaps
+    }
+
+    public void render()
+    {
+        
+    }
+}
